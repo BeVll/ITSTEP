9b244b1898a551b0f86d9e192e9ad9033df29e33
BeVl <vladixerplay@gmail.com>
2022-12-19 21:20:37 +0200

    lab

--- js/30scr.js
+++ js/30scr.js
@@ -23,13 +23,43 @@ function Task3(dig1, dig2, dig3){
    return Number(sum);
 }
 
-function Task4(l){
-    return l*l;
+
+
+function Task4(){
+    if(arguments.length == 1){
+        return arguments[0]*arguments[0];
+    }
+    else{
+        return arguments[0]*arguments[1];
+    }
+}
+function Task5(number) {
+    let tmp = 0;
+    for(var i=1;i<=number/2;i++) {
+        if(number%i === 0) {
+            tmp += i;
+        }
+    }
+
+    if(tmp == number && tmp != 0) {
+        return true;
+    }  
+    else {
+        return false;
+    }  
+}
+function Task6(min, max){
+    let arr = [];
+    for (let i = min; i <= max; i++) {
+        if(Task5(i)){
+            arr.push(i);
+        }
+        
+    }
+    console.log(arr);
 }
 
-function Task4(a, b){
-    return a*b;
+function Task7(h, m, s){
+    
 }
-
-
-alert(Task4(4));
+Task6(1, 10000);
